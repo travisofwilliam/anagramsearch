e8055ac9473ecdf91b1f56d51779237817cfd02e
travaScriptES7 <32422093+travaScriptES7@users.noreply.github.com>
2019-08-25 21:45:03 -0700

    Adding `build` to source control

---
+++ client/build/precache-manifest.7b738b88e3b281e8c0b48ec49378b15f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d810979e43f9aa397413168cdf007514",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e2c434201abbcc2f298",
+    "url": "/static/css/2.d7a94725.chunk.css"
+  },
+  {
+    "revision": "7e2c434201abbcc2f298",
+    "url": "/static/js/2.4d49a67a.chunk.js"
+  },
+  {
+    "revision": "2e381947f104d0187b35",
+    "url": "/static/js/main.75b7a82c.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  }
+]);
